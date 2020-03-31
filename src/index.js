e944bf3b8321e73e92304f3a6dda6c7835021580
allison_johnson <allison.cuttler@gmail.com>
2020-03-30 20:53:57 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,39 @@ document.addEventListener("DOMContentLoaded", () => {
       toyForm.style.display = "none";
     }
   });
-});
+
+  //Fetch Andy's toys: make a GET request to server
+  //With response data, make a <div class="card"> for each toy
+  //Add it to the toy-collection <div>
+  fetch("http://localhost:3000/toys").then(function(response) {
+    return response.json();
+  })
+  .then(function(object){
+    // console.log(object);
+    // debugger
+    toyCollection = document.getElementById("toy-collection");
+    for (const key in object) {
+      //Create new div with class 'card'
+      let newDiv = document.createElement('div');
+      newDiv.className = 'card';
+      toyCollection.appendChild(newDiv);
+
+      //Add toy's name to the card
+      let toyName = document.createElement('h2');
+      toyName.innerHTML = object[key]["name"];
+      newDiv.appendChild(toyName);
+
+      //Add toy's image to the card
+      let toyImg = document.createElement('img');
+      toyImg.className = 'toy-avatar';
+      toyImg.src = object[key]["image"];
+      newDiv.appendChild(toyImg);
+
+      //Add # of likes to the card
+      let numLikes = document.createElement('p');
+      numLikes.innerText = `${object[key]["likes"]} likes`;
+      newDiv.appendChild(numLikes);
+    }//for each key in returned JSON object
+  })
+
+});//event listener DOMContentLoaded
